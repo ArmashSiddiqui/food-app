fcc2945f2a98c4466eaaf0eb8400d7e69e0bdcdd
ArmashSiddiqui <157289829+ArmashSiddiqui@users.noreply.github.com>
2024-09-03 00:33:06 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "food-app-backend-pxct.onrender.com"
+    const url = "https://food-app-backend-pxct.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
